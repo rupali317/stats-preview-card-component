bd03205b02d78939adbd9435fe5aaaed52d892a9
PriyankaRC16 <rupali317@gmail.com>
2023-12-27 13:20:36 +0800

    Added the footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Footer = () => (
+  <small>
+    Challenge by{" "}
+    <a
+      href="https://www.frontendmentor.io?ref=challenge"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Frontend Mentor.
+    </a>{" "}
+    Coded by{" "}
+    <a
+      href="https://www.linkedin.com/in/rupali-rc/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Rupali Roy Choudhury
+    </a>
+  </small>
+);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Footer } from "./components/Footer";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const main = ReactDOM.createRoot(document.getElementById("main"));
+const footer = ReactDOM.createRoot(document.getElementById("footer"));
+
+main.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+footer.render(
+  <React.StrictMode>
+    <Footer />
+  </React.StrictMode>
+);
+
+reportWebVitals(console.log());
